cd5d2fb7e2e3a0af3b41fe81dc0743de4bcd4981
aoliveira <aoliveira@keener.io>
2022-02-17 17:58:30 -0300

    Init pinia state management

--- src/store/shopStore.js
+++ src/store/shopStore.js
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia'
+import { defineStore, acceptHMRUpdate } from 'pinia'
 
 export const shopStore = defineStore('market', {
     state: () => {
@@ -6,6 +6,27 @@ export const shopStore = defineStore('market', {
             market: 24,
             cart: 0
         }
+    },
+    getters:{
+        avaliable(state){
+            return state.market - state.cart
+        }
+    },
+    actions:{
+        addToCart(item){
+            this.cart += item
+            
+        },
+        removeFromCard(item){
+            if(item > this.cart){
+                this.cart = 0
+            }else{
+             this.cart -= item   
+            }
+        }
     }
-
 })
+
+if(import.meta.hot){
+    import.meta.hot.accept(acceptHMRUpdate(shopStore, import.meta.hot))
+}
